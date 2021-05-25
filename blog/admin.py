1794adeab3221f02dd9cfd3c41ab858b5a0ff03e
Omer Cengiz <omercengiz607@gmail.com>
2021-05-26 02:13:49 +0300

    Category Model is created

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from blog.models import CategoryModel
 # Register your models here.
+
+admin.site.register(CategoryModel)
+
---
+++ blog/models/__init__.py
@@ -0,0 +1,1 @@
+from .categoryy import CategoryModel
